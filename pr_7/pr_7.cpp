95defbcca814244448d490620ad037538d40e01d
AniriT1 <irina.kim.6556695@gmail.com>
2023-12-27 13:57:13 +0300

    Добавьте файлы проекта.

---
+++ pr_7/pr_7.cpp
@@ -0,0 +1,38 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h> 
+#include <ctype.h>
+#include <vector>
+
+// Задача про слова
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string input;
+    getline(cin, input);
+
+    vector <string> str;
+    str.push_back(input);
+    vector <int> p;
+    for (int i = 0; i < str.size(); i++) {
+        if (str[i] == " ") {
+            int a = i;
+            p.push_back(a);
+        }
+    }
+
+
+    for (int i = 0; i < p.size(); i++) {
+        cout << p[i];
+    }
+
+    system("pause");
+    return 0;
+}
